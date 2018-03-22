fbdb82b8a249c785f5413669635c91d218c0d63b
Elizaveta Lavelina <lavelinae@gmail.com>
2018-03-22 23:53:31 +0300

    Implemented MemoryMappedFileRead

--- src/MemoryMappedFileExample.java
+++ src/MemoryMappedFileExample.java
@@ -1,39 +1,22 @@
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.Random;
 
 public class MemoryMappedFileExample {
-    static long length = 536870912; //2Gb
 
     public static void main(String[] args) throws Exception {
-        MappedByteBuffer out = new RandomAccessFile("OutputFile.txt", "rw")
-                .getChannel().map(FileChannel.MapMode.READ_WRITE, 0, length*4-1);
-        for (int i = 0; i < length; i++) {
-            byte[] bigInteger = bigIntGenerator().toByteArray();
-
-            if (bigInteger[0] == 0) {
-                byte[] tmp = new byte[bigInteger.length - 1];
-                System.arraycopy(bigInteger, 1, tmp, 0, tmp.length);
-                bigInteger = tmp;
-            }
-
-            for (int j = 0; j < bigInteger.length; j++) {
-                //System.out.println(bigInteger[j]);
-                out.put(bigInteger[j]);
-            }
-        }
-        System.out.println("Finished writing");
-    }
-
-    private static BigInteger bigIntGenerator() {
         Random rnd = new Random();
-        return new BigInteger(32, rnd);
+        MappedByteBuffer out = new RandomAccessFile("javatext.txt", "rw")
+                .getChannel().map(FileChannel.MapMode.READ_WRITE, 0, 2147483647); //размер файла в байтах
+        for (int i = 0; i < 536870911; i++) //сколько раз положить переменную int
+        {
+            int a = rnd.nextInt();
+            out.putInt(a);
+            //System.out.println(a);
+        }
+
+        System.out.println("Finished writing");
+
     }
 }
-
-
-
-
--- src/MemoryMappedFileRead.java
+++ src/MemoryMappedFileRead.java
@@ -5,34 +5,37 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 public class MemoryMappedFileRead {
-    private static String BigTextFile = "OutputFile.txt";
+    private static String bigFile = "javatext.txt";
 
     public static void main(String[] args) throws Exception {
         //Create file object
-        File TextFile = new File(BigTextFile);
+        File file = new File(bigFile);
 
         //Get file channel in readonly mode
-        FileChannel TextFileChannel = new RandomAccessFile(TextFile, "rw").getChannel();
+        FileChannel fileChannel = new RandomAccessFile(file, "r").getChannel();
 
         //Get direct byte buffer access using channel.map() operation
-        MappedByteBuffer TextBuffer = TextFileChannel.map(FileChannel.MapMode.READ_WRITE, 0, TextFileChannel.size());
+        MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
 
         // the buffer now reads the file as if it were loaded in memory.
-        // System.out.println(TextBuffer.isLoaded());  //prints false
-        //System.out.println(TextBuffer.capacity());  //Get the size based on content size of file
+        System.out.println(buffer.capacity());  //Get the size based on content size of file
+        System.out.println(buffer.limit());
 
+        long a = 0;
+        long b = 0;
+        BigInteger sum = BigInteger.valueOf(0);
         //You can read the file from this buffer the way you like.
-        BigInteger Result = BigInteger.valueOf(0);
-        for (int i = 0; i < 536870912; i++) {
-            byte[] byteArray = new byte[4];
-            for (int j = 0; j < 4; j++){
-                byteArray[j] = TextBuffer.get();
+        for (int i = 0; i < 536870911; i++) {
+
+            b = 0;
+            for (int j = 0; j < 4; j++) {
+                a = buffer.get() << (8 * j);
+                b = a | b;
             }
-            BigInteger bigInteger = new BigInteger(byteArray);
-            //BigInteger num = BigInteger.valueOf(TextBuffer.getInt());
-            System.out.println(bigInteger);
-            Result = Result.add(bigInteger);
+            BigInteger temp = BigInteger.valueOf(b);
+            sum = sum.add(temp);
         }
-        System.out.println(Result);
+        System.out.println(sum); //Print the sum
+
     }
 }
